aaabe322f010d744c7141d702f6b54325cf8331a
KonstantinHerasimenko <152262672+KonstantinHerasimenko@users.noreply.github.com>
2024-10-26 17:23:45 +0300

    Update page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,80 +1,40 @@
 'use client'
 import Input from '@/components/ui/input'
+import Label from '@/components/ui/label'
+import { cn } from '@/utils/css'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { FormEvent } from 'react'
 
 export default function Auth() {
-	async function onSubmit(event: FormEvent<HTMLFormElement>) {
-		event.preventDefault()
-		try {
-			const formData = new FormData(event.currentTarget)
-			const login = formData.get('login')
-			const password = formData.get('password')
-			const response = await fetch(`http://100.126.9.5/api/Account/token`, {
-				method: 'POST',
+	@@ -17,7 +16,7 @@ export default function Auth() {
 				headers: {
 					'Content-Type': 'application/json; charset=utf-8',
 				},
-				body: JSON.stringify({login,password}),
+				body: JSON.stringify({ login, password }),
 			})
 			//`{"login":"${formData.get('login')}","password":"${formData.get('password')}"}`
 
-			// Handle response if necessary
-			const data = await response.json()
-			console.log(data)
-		} catch (error) {
-			// Handle error if necessary
-			console.error(error)
-		}
+	@@ -31,26 +30,36 @@ export default function Auth() {
 	}
 
 	return (
-		<main className="flex justify-center items-center h-screen">
+		<main className={cn('flex justify-center items-center', 'h-screen')}>
 			<form
-				className="text-center w-72.25"
+				className={cn('text-center', 'w-72.25')}
 				onSubmit={onSubmit}>
 				<div
 					role="group"
-					className="rounded-t-2.5xl px-4.5 pt-4.5 bg-DarkBGRD pb-3">
+					className={cn('rounded-t-2.5xl', 'px-4.5 pt-4.5 pb-3', 'bg-DT_TextboxEText')}>
 					<h1 className="text-lg font-semibold">С возвращением!</h1>
 					<h2 className="text-sm">Мы так рады видеть вас снова!</h2>
-					<label
-						className="text-start mb-1"
-						htmlFor="login">
-						<h2 className="text-DarkTextMissStyle text-sm ml-1 pt-2">Логин</h2>
-						<Input
-							className="w-full"
-							type="text"
-							name="login"
-						/>
-					</label>
-					<label
-						className="text-start"
-						htmlFor="password">
-						<h2 className="text-DarkTextMissStyle text-sm ml-1 pt-1">Пароль</h2>
-						<Input
-							className="w-full"
-							type="password"
-							name="password"
-						/>
-					</label>
-					<h3 className="text-xs pt-2 text-DarkTextMissStyle">
-						Нужна учётная запись?{' '}
-						<Link
-							href={'/'}
-							className="text-Blue">
-							Зарегистрироваться
-						</Link>
-					</h3>
-				</div>
-
-				<button
-					type="submit"
-					className="bg-[rgb(45,_45,_45)] w-full text-Blue rounded-b-2.5xl py-2">
-					Вход
-				</button>
-			</form>
-		</main>
-	)
-}
+					<Label
+						name="login"
+						type="text"
+						placeholder="Логин"
+					/>
+					<Label
+						name="password"
+						type="password"
+						placeholder="Пароль"
+					/>
+					<h3 className={cn('text-xs text-DT_Login', 'pt-2')}>
