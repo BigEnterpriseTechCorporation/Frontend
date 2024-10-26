cdf475dd243bdc08b42c8e25ec83cc2458f19b8e
Marvan4k <romanmarvanov@gmail.com>
2024-10-26 12:54:47 +0300

    feat: refactor editBar

--- components/ui/editBar.tsx
+++ components/ui/editBar.tsx
@@ -3,26 +3,28 @@ import ReactQuill from 'react-quill-new'
 import 'react-quill-new/dist/quill.snow.css'
 
 export default function EditBar() {
-	const modules = {
-		toolbar: [
-			[{ font: [] }],
-			[{ size: [] }],
-			[{ color: [] }],
-			['bold', 'italic', 'underline', 'strike', 'blockquote'],
-			[{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-			['link', 'image', 'video'],
-			['clean'],
-		],
-		clipboard: {
-			// toggle to add extra line breaks when pasting HTML:
-			matchVisual: false,
-		},
-	}
-	return (
-		<ReactQuill
-			modules={modules}
-			theme="snow"
-			className="bg-DarkTextboxBGRD h-full grid grid-rows-[max-content_auto]"
-		/>
-	)
+    const modules = {
+        toolbar: [
+          [{ font: [] }],
+          [{ size: [] }],
+          [{color: []}],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [
+            { list: 'ordered' },
+            { list: 'bullet' },
+            { indent: '-1' },
+            { indent: '+1' },
+          ],
+          ['link', 'image', 'video'],
+          ['clean'],
+        ],
+        clipboard: {
+          // toggle to add extra line breaks when pasting HTML:
+          matchVisual: false,
+        },
+      }
+    return (
+        <ReactQuill modules={modules} theme="snow" className="bg-DarkTextboxBGRD h-full grid grid-rows-[max-content_auto]"/>
+    );
 }
+
