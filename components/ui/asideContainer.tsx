3dbb939a0a8e3adab441903e33c5fc06b9b27268
ViteAAA <147093510+ViteAAA@users.noreply.github.com>
2024-10-27 13:54:23 +0300

    last changhe

--- components/ui/asideContainer.tsx
+++ components/ui/asideContainer.tsx
@@ -14,7 +14,7 @@ export default function AsideContainer({ children, className, rootClassName, roo
 			className={cn(rootClassName,'fixed z-20 top-0 left-0', 'w-screen h-screen', 'bg-[rgba(4,4,4,0.6)]')}
 			onClick={rootOnClick}>
 			<div
-				className={cn(className, 'p-6', 'bg-DT_Backl', filter ? 'rounded-2xl' : 'rounded-md', filter ? 'w-11/12 md:w-3/5' : 'w-11/12 md:w-3/4 sm:!h-[30%] !h-11/12', 'h-5/6')}
+				className={cn(className, filter ? 'p-6' : 'p-0', 'bg-DT_Backl', 'rounded-2xl', filter ? 'w-11/12 md:w-3/5' : 'w-11/12 md:max-w-[30%] sm:!h-[40%] ', 'h-5/6')}
 				{...props}
 				onClick={onClick}>
 				{children}
--- components/ui/filters.tsx
+++ components/ui/filters.tsx
@@ -5,16 +5,24 @@ export default function Filters() {
     return (
         <AsideContainer rootClassName='flex items-center justify-center ' filter={false}>
             <div className="">
-                <h1 className="text-3xl mb-5 text-center lg:mb-12">
+                <h1 className="text-3xl mb-5 text-center lg:mb-12 bg-DT_BacklLable rounded-t-2xl pt-3 pb-2">
                     Фильтры
                 </h1>
-                <div className="lg:flex lg:justify-between lg:items-center">
-                    <div className="lg:h-fit lg:ml-0 md:ml-[50%] lg:-translate-x-0 md:-translate-x-1/2">
+                <div className="lg:mt-0 mt-12 mb-6 ">
+                        <h2 className="text-xl mb-2 text-center">
+                            Фильтр по фамилиям
+                        </h2>
+                        <div className="ml-[50%] -translate-x-1/2 ">
+                            <Input className="lg:w-full  w-full " />
+                        </div>
+                </div>
+                <div className="">
+                    <div className="lg:h-fit ">
                         <h2 className="text-xl mb-2 text-center sm:mt-0 mt-20">
-                            Дата
+                            Фильтр по датам
                         </h2>
                         {/* кнопки фильтрации */}
-                        <div className="sm:flex sm:mt-0 mt-4">
+                        <div className="sm:flex sm:mt-0 mt-4 ml-[50%] -translate-x-1/2">
                             <Input className="w-full" />
                             <p className="sm:mx-3 text-center sm:my-0 my-1">
                                 по
@@ -25,15 +33,10 @@ export default function Filters() {
                             */}
                         </div>
                     </div>
-                    <div className="lg:w-[40%] lg:mt-0 mt-12 sm:mt-6 lg:ml-0 md:ml-[50%] lg:-translate-x-0 md:-translate-x-1/2">
-                        <h2 className="text-xl mb-2 text-center">
-                            Фамилия
-                        </h2>
-                        <div className="flex">
-                            <Input className="lg:w-full  w-full" />
-                        </div>
-                    </div>
                 </div>
+                <button className="ml-[50%] -translate-x-1/2 mt-4 bg-DT_BacklLable text-Blue py-2 px-9 rounded-full">
+                    Применить фильтры
+                </button>
             </div>
 
         </AsideContainer>
