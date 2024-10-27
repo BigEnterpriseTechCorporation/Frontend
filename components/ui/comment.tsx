6000b7cf750be6a35465e1619a5b8a38d4270379
KonstantinHerasimenko <konstantin.herasimenko@gmail.com>
2024-10-27 07:11:59 +0300

    fix: fixed full task responisevness

--- components/ui/comment.tsx
+++ components/ui/comment.tsx
@@ -9,7 +9,7 @@ interface CommentProps extends ComponentPropsWithoutRef<'div'> {
 export default function Comment({ author, text, ...props }: CommentProps) {
 	return (
 		<div
-			className={cn('bg-DT_TextboxCont', 'self-stretch', 'mb-4 sm:mb-0', 'sm:rounded-3xl', 'p-3')}
+			className={cn('bg-DT_TextboxCont', 'self-stretch', 'mb-4 sm:mb-0', 'sm:rounded-3xl rounded-2xl', 'p-3')}
 			{...props}>
 			<h4 className={cn("text-lg","mb-1")}>{author}</h4>
 			<h5 className="text-sm">{text}</h5>
