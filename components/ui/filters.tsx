7fd205ce72b062a19ff6925f9b07586bc462306a
KonstantinHerasimenko <konstantin.herasimenko@gmail.com>
2024-10-27 13:52:20 +0300

    Revert "fat: add adapt to filters"
    This reverts commit e65c2994c56f0c0a665c4a4b84eb236efba211a0.

--- components/ui/filters.tsx
+++ components/ui/filters.tsx
@@ -3,38 +3,32 @@ import Input from './input'
 
 export default function Filters() {
     return (
-        <AsideContainer rootClassName='flex items-center justify-center ' filter={false}>
-            <div className="">
-                <h1 className="text-3xl mb-5 text-center lg:mb-12">
-                    Фильтры
-                </h1>
-                <div className="lg:flex lg:justify-between lg:items-center">
-                    <div className="lg:h-fit lg:ml-0 md:ml-[50%] lg:-translate-x-0 md:-translate-x-1/2">
-                        <h2 className="text-xl mb-2 text-center sm:mt-0 mt-20">
-                            Дата
-                        </h2>
-                        {/* кнопки фильтрации */}
-                        <div className="sm:flex sm:mt-0 mt-4">
-                            <Input className="w-full" />
-                            <p className="sm:mx-3 text-center sm:my-0 my-1">
-                                по
-                            </p>
-                            <Input className="w-full" />
-                            {/*
-                                сдесь должна быть какая-то функция фильтрации по дате, но у нас нет даже дат
-                            */}
-                        </div>
-                    </div>
-                    <div className="lg:w-[40%] lg:mt-0 mt-12 sm:mt-6 lg:ml-0 md:ml-[50%] lg:-translate-x-0 md:-translate-x-1/2">
-                        <h2 className="text-xl mb-2 text-center">
-                            Фамилия
-                        </h2>
-                        <div className="flex">
-                            <Input className="lg:w-full  w-full" />
-                        </div>
-                    </div>
-                </div>
+        <AsideContainer rootClassName='flex items-start justify-center ' filter={false}>
+            <h1 className="text-3xl mb-5">
+                Фильтры
+            </h1>
+            <h2 className="text-xl mb-2">
+                Дата
+            </h2>
+            <div className="flex">
+                <Input />
+                <p className="mx-3">
+                    по
+                </p>
+                <Input />
+                {/*
+                    сдесь должна быть какая-то функция фильтрации по дате, но у нас нет даже дат
+                */}
             </div>
+            <h2 className="text-xl mb-2 mt-6">
+                Фамилия
+            </h2>
+            <div className="flex">
+                <Input />
+                
+            </div>
+            {/* форма с фильтрами */}
+            {/* кнопки фильтрации */}
 
         </AsideContainer>
     )
