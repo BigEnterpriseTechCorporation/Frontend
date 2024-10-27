f18d401d11dc7a456dfa8193f63aaea3ddb3f064
KonstantinHerasimenko <konstantin.herasimenko@gmail.com>
2024-10-27 09:17:12 +0300

    feat: add user tasks

--- hooks/useUserTasks.tsx
+++ hooks/useUserTasks.tsx
@@ -2,18 +2,33 @@
 import { host } from '@/config'
 import { useEffect, useState } from 'react'
 
+interface state {
+	result: number[]
+	id: number
+	exception: null
+	status: number
+	isCanceled: boolean
+	isCompleted: true
+	isCompletedSuccessfully: boolean
+	creationOptions: number
+	asyncState: null
+	isFaulted: boolean
+}
+
 export default function useUserTasks(): number[] {
 	const [state, setState] = useState<number[]>([])
 
 	useEffect(() => {
 		try {
-
-			fetch(`http://${host}/api/Users/${JSON.parse(localStorage.getItem("user")!).id}`, {
+			fetch(`http://${host}/api/Account/tasks`, {
 				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
 			})
 				.then(res => res.json())
-				.then((data: number[]) => {
-					setState(data)
+				.then((data: state) => {
+					setState(data.result)
 				})
 				.catch(error => setState(error.toString()))
 		} catch (error) {
