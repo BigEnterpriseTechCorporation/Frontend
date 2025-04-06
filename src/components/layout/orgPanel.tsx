9435790106e0a1b52a20085f5ffa268dcd215bf0
Marvan4k <romanmarvanov@gmail.com>
2025-04-06 04:44:49 +0300

    add addRequest

--- src/components/layout/orgPanel.tsx
+++ src/components/layout/orgPanel.tsx
@@ -4,10 +4,11 @@ import Image from "next/image"
 import { useTheme } from '@/context/ThemeContext'
 import MainOrg from "./organisator/mainOrg"
 import AddRequest from "./organisator/addRequest"
+import CurrentRequest from "./organisator/currentRequest"
 
 export default function OrgPanel(){
     const { isDarkTheme } = useTheme()
-    
+
     return (
         <div className="max-h-screen w-full  h-[94vh]  flex  justify-center overflow-hidden ">
             <div className={`${isDarkTheme ? 'bg-[#43434375] border-[#3E4147]' : 'bg-[#6B6B6B75] border-[#D8DBDF]'} rounded-3xl w-full border-2 `}>
@@ -18,7 +19,43 @@ export default function OrgPanel(){
                     </div>
                 </div>
                 <div className="max-h-[70vh] h-full">
-                    <AddRequest />
+                    <CurrentRequest requests={[
+                        {
+                            title: 'Событие',
+                            description: '(Краткое описание События)',
+                            author: 'Фамилия И. Ник'
+                        },
+                        {
+                            title: 'Событие',
+                            description: '(Краткое описание События)',
+                            author: 'Фамилия И. Ник'
+                        },
+                        {
+                            title: 'Событие',
+                            description: '(Краткое описание События)',
+                            author: 'Фамилия И. Ник'
+                        },
+                        {
+                            title: 'Событие',
+                            description: '(Краткое описание События)',
+                            author: 'Фамилия И. Ник'
+                        },
+                        {
+                            title: 'Событие',
+                            description: '(Краткое описание События)',
+                            author: 'Фамилия И. Ник'
+                        },
+                        {
+                            title: 'Событие',
+                            description: '(Краткое описание События)',
+                            author: 'Фамилия И. Ник'
+                        },
+                        {
+                            title: 'Событие',
+                            description: '(Краткое описание События)',
+                            author: 'Фамилия И. Ник'
+                        }
+                    ]} />
                 </div>
             </div>
         </div>
